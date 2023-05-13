e1897509a99d5248a29e155cb15037ae99c3269f
Zohar-Ifrah <129209726+Zohar-Ifrah@users.noreply.github.com>
2023-05-14 02:34:19 +0300

    Delete views directory

--- views/home.jsx
+++
@@ -1,8 +0,0 @@
-export function Home() {
-    return (
-        <section className="home">
-            <h1>Welcome To Miss Book</h1>
-            <img src="../assets/img/react.png" alt="" /> 
-        </section>
-    )
-}
